f9aef99146abb6f6544c801b1e8af9cc22798cab
Abhijit Pradhan <abhijit@architizer.com>
2015-09-17 12:28:29 -0400

    adds Feature, Test, and Broswer models, and defines their relationships

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+	name = models.CharField(max_length=200)
+
+class Feature(models.Model):
+	project = models.ForeignKey(Project)
+	name = models.CharField(max_length=200)
+
+class Test(models.Model):
+	feature = models.ForeignKey(Feature)
+	description = models.CharField(max_length=200)
+
+class Browser(models.Model):
+	test = models.ForeignKey(Test)
+	name = models.CharField
+	version = models.CharField(max_length=200)
+	operating_system = models.CharField(max_length=200)
