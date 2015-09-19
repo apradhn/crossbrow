9f028a01d755976bc0df6935165e6c4b51b97705
Abhijit Pradhan <abhijit@architizer.com>
2015-09-19 11:27:47 -0400

    migrates models

---
+++ projects/migrations/0001_initial.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Browser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('version', models.CharField(max_length=200)),
+                ('operating_system', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Feature',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Project',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Test',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('description', models.CharField(max_length=200)),
+                ('feature', models.ForeignKey(to='projects.Feature')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='feature',
+            name='project',
+            field=models.ForeignKey(to='projects.Project'),
+        ),
+        migrations.AddField(
+            model_name='browser',
+            name='test',
+            field=models.ForeignKey(to='projects.Test'),
+        ),
+    ]
