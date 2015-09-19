3e2669a5fb13e4ac909d11e076b027c925eda64f
apradhn <abhijit.pradhan6@gmail.com>
2015-09-19 15:56:05 -0400

    Adds choices to Browser name field

--- projects/models.py
+++ projects/models.py
@@ -25,8 +25,21 @@ class Test(models.Model):
 
 
 class Browser(models.Model):
+    CHROME = 'C',
+    FIREFOX = 'FF',
+    IE = 'IE'
+    SAFARI = 'S'
+    BROWSER_CHOICES = (
+        (CHROME, 'Chrome'),
+        (FIREFOX, 'Mozilla Firefox'),
+        (IE, 'Internet Explorer'),
+        (SAFARI, 'Safari')
+    )
+
     project = models.ForeignKey(Project, null=True)
-    name = models.CharField(max_length=200, default='browser name')
+    name = models.CharField(max_length=200,
+                            choices=BROWSER_CHOICES,
+                            default=CHROME)
     version = models.CharField(max_length=200)
     operating_system = models.CharField(max_length=200)
 
