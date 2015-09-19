860cfe568344e1b084a954b2c53f2aa32aa59b98
apradhn <abhijit.pradhan6@gmail.com>
2015-09-19 15:29:07 -0400

    adds Feature inline fields to Project Admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Feature, Browser, Test
+
+
+class FeatureInline(admin.TabularInline):
+    model = Feature
+    extra = 3
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [FeatureInline]
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Feature)
+admin.site.register(Browser)
+admin.site.register(Test)
