604a4ca1a8bc794a7d0c9771e0f4448c4e9107cb
apradhn <abhijit.pradhan6@gmail.com>
2015-09-19 22:34:04 -0400

    fixes url regex for /products/<product_id>/features

--- projects/urls.py
+++ projects/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
     # /projects/<project_id>/browsers/
     url(r'(?P<project_id>[0-9]+)/browsers/$', views.browsers_index,
         name='browsers_index'),
+    # /projects/<project_id>/features/
+    url(r'(?P<project_id>[0-9]+)/features/$', views.features_index,
+        name='features_index'),
     # /projects/<project_id>/browsers/<browser_id>
     url(r'(?P<project_id>[0-9]+)/browsers/(?P<browser_id>[0-9]+)/$',
         views.browser_detail, name='browser_detail'),
-    # /projects/<project_id>/features/
-    url(r'(?P<project_id>[0-9]+)/features$', views.features_index,
-        name='features_index'),
     # /projects/<project_id>/
     url(r'(?P<project_id>[0-9]+)/$', views.project_detail,
         name='project_detail'),
