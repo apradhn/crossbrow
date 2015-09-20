4b8beac8fe62a679a539725339f2efadaed2cfc3
apradhn <abhijit.pradhan6@gmail.com>
2015-09-19 20:09:31 -0400

    Moves Test model from /projects to /test-cases

--- projects/models.py
+++ projects/models.py
@@ -6,18 +6,3 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# class Test(models.Model):
-#     PASS = 'P'
-#     FAIL = 'F'
-#     RESULTS = (
-#         (PASS, 'Pass'),
-#         (FAIL, 'Fail')
-#     )
-#     feature = models.ForeignKey(Feature)
-#     description = models.CharField(max_length=200)
-#     result = models.CharField(max_length=10, blank=True, choices=RESULTS)
-
-#     def __str__(self):
-#         return self.description
--- test_cases/models.py
+++ test_cases/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from features.models import Feature
 
-# Create your models here.
+
+class Test(models.Model):
+    PASS = 'P'
+    FAIL = 'F'
+    RESULTS = (
+        (PASS, 'Pass'),
+        (FAIL, 'Fail')
+    )
+    feature = models.ForeignKey(Feature)
+    description = models.CharField(max_length=200)
+    result = models.CharField(max_length=10, blank=True, choices=RESULTS)
+
+    def __str__(self):
+        return self.description
